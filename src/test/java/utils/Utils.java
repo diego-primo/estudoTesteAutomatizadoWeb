e050c08079ec88dfd9ea7315a06ce74741a80863
diego-primo <diego.pg.dias@gmail.com>
2021-05-12 08:14:27 -0300

    Teste automatizado da tela login

---
+++ src/test/java/utils/Utils.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Utils {
+	
+	public static WebDriver driver;
+
+	public static void acessarNavegador() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+
+	}
+	
+	public static void fecharNavegador() {
+		driver.quit();
+
+	}
+	
+	public static <T> T Na(Class<T> classe) {
+		return PageFactory.initElements(driver, classe);
+	}
+}
