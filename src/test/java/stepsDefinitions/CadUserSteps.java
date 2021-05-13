d47edb2ec3f95ac5322cc55ac67056320022fbd0
diego-primo <diego.pg.dias@gmail.com>
2021-05-13 12:44:08 -0300

    Desenvolvimento o teste na tela de cadastro... Incompleto!

--- src/test/java/stepsDefinitions/CadUserSteps.java
+++ src/test/java/stepsDefinitions/CadUserSteps.java
@@ -1,5 +1,71 @@
 package stepsDefinitions;
 
+import static utils.Utils.*;
+
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import pageObjects.CadUserPage;
+
 public class CadUserSteps {
 
+	@Quando("mover o mouse para Aba Admin")
+	public void moverOMouseParaAbaAdmin() {
+		Na(CadUserPage.class).moverMouseAbaAdmin();
+	}
+
+	@Quando("mover o mouse para Aba User Management")
+	public void moverOMouseParaAbaUserManagement() {
+		Na(CadUserPage.class).moverMouseAbaUserManagement();
+	}
+
+	@Quando("mover o mouse para Aba User e depois clicar nele")
+	public void moverOMouseParaAbaUserEDepoisClicarNele() {
+		Na(CadUserPage.class).moverMouseAbaUserEClick();
+	}
+
+	@Quando("clicar no botao Add")
+	public void clicarNoBotaoAdd() {
+		Na(CadUserPage.class).clicarBotaoAdd();
+	}
+
+	@Quando("confirmar se o campo o User Role esta selecionado com {string}")
+	public void confirmarSeOCampoOUserRoleEstaSelecionadoCom(String string) {
+		Na(CadUserPage.class).validarCampoUserRole(string);
+	}
+
+	@Quando("informar o Employee Name {string}")
+	public void informarOEmployeeName(String string) {
+		Na(CadUserPage.class).informarCampoEmployeeName(string);
+	}
+
+	@Quando("informar Username {string}")
+	public void informarUsername(String string) {
+		Na(CadUserPage.class).informarCampoUserName(string);
+	}
+
+	@Quando("confirmar se o campo o  Status esta selecionado {string}")
+	public void confirmarSeOCampoOStatusEstaSelecionado(String string) {
+		Na(CadUserPage.class).validarCampoStatus(string);
+	}
+
+	@Quando("informar o Password {string}")
+	public void informarOPassword(String string) {
+		Na(CadUserPage.class).informarPassword(string);
+
+	}
+
+	@Quando("informar o Confirm Password {string}")
+	public void informarOConfirmPassword(String string) {
+		Na(CadUserPage.class).informarConfirmPassword(string);
+	}
+
+	@Quando("clicar botao Save")
+	public void clicarBotaoSave() {
+		Na(CadUserPage.class).clicarBotaoSalvar();
+	}
+
+	@Entao("o sistema confirmara o cadastrado realizado com sucesso {string}")
+	public void oSistemaConfirmaraOCadastradoRealizadoComSucesso(String string) {
+		Na(CadUserPage.class).validarCadastroUsuarioComSucesso(string);
+	}
 }
--- target/test-classes/features/CadUser.feature
+++ target/test-classes/features/CadUser.feature
@@ -2,11 +2,13 @@
 #@enconding: UFT-8
 
 @CadastroUsuario
-Funcionalidade: Acessar Sistema
+Funcionalidade: Acessar Sistema ja logado
 
   @CadastroComSucesso
-  Cenario: Cadastrado de Usuario com Sucesso
-    Quando eu clicar no modulo Admin
+  Cenario: Cadastrado de Usuario com Sucesso 
+    Quando mover o mouse para Aba Admin
+    E mover o mouse para Aba User Management
+    E mover o mouse para Aba User e depois clicar nele
     E clicar no botao Add
     E confirmar se o campo o User Role esta selecionado com "ESS"
     E informar o Employee Name "Dominic Chase"
